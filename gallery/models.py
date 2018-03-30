7a6967a8a309aaa9c477588f303719f2eec6412e
nikkie <takuyafjp+develop@gmail.com>
2018-03-30 12:22:58 +0900

    [feat] auto_incrementさせる想定のフィールドを削除
    * `python manage.py makemigrations gallery` したところエラーで落ちた。
    > AssertionError: A model can't have more than one AutoField.
    * Django は各モデルに対して id という名前の主キーフィールドを自動的に設定しているという情報が見つかった。
    https://qiita.com/ykiu/items/c288b99d0a1956e8ac9f
    * AutoFieldのドキュメントにも「通常は直接使う必要はありません」と書かれている。
    https://docs.djangoproject.com/ja/1.11/ref/models/fields/#django.db.models.AutoField
    * エラーで落ちた原因はAutoFieldを設けたことと考え、該当フィールドを削除した。

--- gallery/models.py
+++ gallery/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 class MosaicArt(models.Model):
-    ma_id = models.AutoField()
     user = models.ForeignKey('auth.User')
     file_name = models.CharField(max_length=200)
     original_image = models.CharField(max_length=200)
