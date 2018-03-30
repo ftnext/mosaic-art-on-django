87104fd6867445905664b67f07b438358dcb9906
nikkie <takuyafjp+develop@gmail.com>
2018-03-30 15:33:38 +0900

    [feat] モザイクアート作成に使う関数をビューに移植
    * モザイクアートのリポジトリの mosaic_art.py から必要な部分をコピーしてきた
        * 必要なモジュールのインポート文を移植
        * モザイクアート作成に使う関数を移植

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,98 @@
+import csv
+
 from django.shortcuts import render
 from .models import MosaicArt
+
+from PIL import Image
+
+from .mosaic_art import calc
+from .mosaic_art import image_process
+
+
+DOT_AREA_ONE_SIDE = 10
+THUMBNAIL_ONE_SIDE = 40
+# 2つの色(R,G,B)の間の最大の距離
+MAX_COLOR_DISTANCE = 255**2 * 3
+# CSVファイル中のカラムの意味づけ
+POS_NAME  = 0
+POS_RED   = 1
+POS_GREEN = 2
+POS_BLUE  = 3
+
 
 def art_list(request):
     mosaic_arts = MosaicArt.objects.order_by('-created_date')
     return render(request, 'gallery/art_list.html', {'mosaic_arts': mosaic_arts})
+
+def materials_list_from_file(filename):
+    """Returns a list which contains material image information.
+
+    Args:
+        filename: File name such as "foo.csv"
+            The file contains information on average color of image.
+            (Average color maens the average of the values of R of all pixels,
+             the average of the values of G of all pixels,
+             and the average of the values of B of all pixels)
+            A row is as follows:
+                image_name, R_average, G_average, B_average
+
+    Returns:
+        A list of tuples
+        Tuple is like (
+            image_name   : str (such as "bar.png"),
+            red_average  : int,
+            green_average: int,
+            blue_average : int
+            )
+    """
+    color_data = []
+    with open(filename, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            image_info = (row[POS_NAME], int(row[POS_RED]),
+                          int(row[POS_GREEN]), int(row[POS_BLUE]))
+            color_data.append(image_info)
+    return color_data
+
+def color_distance(RGB1, RGB2):
+    """Returns color distance
+
+    Considering the distance between two points
+    (x1, y1, z1) and (x2, y2, z2) in three dimensions
+
+    Args:
+        RGB1: A tuple which means (R, G, B)
+        RGB2: A tuple which means (R, G, B)
+
+    Returns:
+        color distance(:int)
+
+    """
+    d2_r = (RGB1[0] - RGB2[0]) ** 2
+    d2_g = (RGB1[1] - RGB2[1]) ** 2
+    d2_b = (RGB1[2] - RGB2[2]) ** 2
+    return d2_r + d2_g + d2_b
+
+def similar_color_filename(average_color, color_data):
+    """Returns name of file similar to average color
+
+    Find the image with average color closest to `average_color` from `color_data`
+
+    Args:
+        average_color: a tuple which means (R, G, B) of average color of a certain range
+        color_data: A list of tuples
+                    Tuple is like (image_name, red_average, green_average, blue_average)
+
+    Returns:
+        A name of file such as 'foo.png' (NOT path)
+    """
+    distance = MAX_COLOR_DISTANCE
+    filename = ''
+    # 色の差が最小になるファイルを決定(距離に見立てている)
+    for color in color_data:
+        sample_color = (color[POS_RED], color[POS_GREEN], color[POS_BLUE])
+        d = color_distance(average_color, sample_color)
+        if d < distance:
+            distance = d
+            filename = color[POS_NAME]
+    return filename
