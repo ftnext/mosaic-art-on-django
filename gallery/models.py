17a79ad7ecfa027d743ee8fa5df29e210bb8353c
nikkie <takuyafjp+develop@gmail.com>
2018-03-30 12:14:43 +0900

    [feat] MosaicArtモデルの作成
    * モザイクアートについて以下の情報をもたせることにした。
    	* 誰が作ったか：user
    	* いつ作ったか：created_date
    	* どのような対象画像から作ったか：original_image
    	* 何というファイル名のモザイクアートか：file_name
    * 通し連番のIDを使う経験があったので、ma_idフィールドを定義

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class MosaicArt(models.Model):
+    ma_id = models.AutoField()
+    user = models.ForeignKey('auth.User')
+    file_name = models.CharField(max_length=200)
+    original_image = models.CharField(max_length=200)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def __str__(self):
+        return self.file_name
